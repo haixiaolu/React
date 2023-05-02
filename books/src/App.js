d5116f0986f8efa709cac269cb9150e260cf51a2
haixiaolu <haixiao.lu-1@ou.edu>
2023-05-01 20:42:13 -0500

    added books app

---
+++ books/src/App.js
@@ -0,0 +1,44 @@
+import { useState } from "react"
+import BookCreate from "./components/BookCreate";
+import BookList from "./components/BookList";
+
+
+function App(){
+
+    const [books, setBooks] = useState([]);
+
+    const editBookById = (id, newTitle) => {
+        const updateBooks = books.map((book) => {
+            if(book.id = id){
+                return {...book, title: newTitle};
+            }
+            return book;
+        });
+        setBooks(updateBooks);
+    };
+
+    const deleteBookById = (id) => {
+        const updatedBooks = books.filter((book) => {
+            return book.id != id;
+        });
+        setBooks(updatedBooks);
+    };
+
+    const createBook = (title) => {
+        const updateBooks = [
+            ...books,
+            {id: Math.round(Math.random() * 9999), title}
+        ];
+        setBooks(updateBooks)
+
+    };
+
+    return(
+         <div className="app">
+            <h1>Reading List</h1>
+            <BookList onEdit={editBookById} onDelete={deleteBookById} books={books} />
+            <BookCreate onCreate={createBook} />
+        </div>
+    )
+}
+export default App;
